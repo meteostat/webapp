5a8c9fd31532b98e68582c2151d3fa991f66c98c
clampr <christian.lamprecht@aol.de>
2021-10-21 14:42:09 +0200

    Node version

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,9 +13,9 @@
           </a>
         </li>
         <li class="d-inline-block ms-3">
-          <router-link to="/insights">
-            {{ t('insights') }}
-          </router-link>
+          <a href="https://medium.com/meteostat">
+            {{ t('blog') }}
+          </a>
         </li>
         <li class="d-block mt-3 my-md-0 ms-md-4">
           <router-link
