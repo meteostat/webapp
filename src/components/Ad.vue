84e8dd240932cb788807295d2707580f57a4e3ce
clampr <christian.lamprecht@aol.de>
2021-11-29 17:14:10 +0100

    Add max height to native ads

--- src/components/Ad.vue
+++ src/components/Ad.vue
@@ -56,6 +56,7 @@ export default defineComponent({
 <style lang="scss" scoped>
 ins.adsbygoogle {
   display: block;
+  max-height: 280px;
 
   &[data-ad-status="unfilled"] {
     display: none !important;
